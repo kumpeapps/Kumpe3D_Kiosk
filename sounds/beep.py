6d5c6d551f43410b53e92d54e267275187fb02fc
Justin Kumpe <jakumpe@kumpes.com>
2024-02-28 18:25:37 -0600

    -

--- sounds/beep.py
+++ sounds/beep.py
@@ -1,11 +1,12 @@
 """Beep Functions"""
+
 import flet as ft
 
 
 def error(page: ft.Page):
     """Play Error Beep"""
     error_file = ft.Audio(
-        src="https://github.com/kumpeapps/Kumpe3D_Kiosk/blob/632ea64ec087451cb44447d62c84a153b7edd1fa/sounds/error.wav?raw=true",
+        src="https://github.com/kumpeapps/Kumpe3D_Kiosk/blob/632ea64ec087451cb44447d62c84a153b7edd1fa/sounds/error.wav?raw=true",  # pylint: disable=line-too-long
         autoplay=True,
     )
     page.overlay.append(error_file)
@@ -14,7 +15,7 @@ def error(page: ft.Page):
 def success(page: ft.Page):
     """Play Success Beep"""
     success_file = ft.Audio(
-        src="https://github.com/kumpeapps/Kumpe3D_Kiosk/blob/632ea64ec087451cb44447d62c84a153b7edd1fa/sounds/success.wav?raw=true",
+        src="https://github.com/kumpeapps/Kumpe3D_Kiosk/blob/632ea64ec087451cb44447d62c84a153b7edd1fa/sounds/success.wav?raw=true",  # pylint: disable=line-too-long
         autoplay=True,
     )
     page.overlay.append(success_file)
