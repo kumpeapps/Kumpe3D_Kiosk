8ca4370931a7bc43b18beac60f7482299382aeec
Justin Kumpe <jakumpe@kumpes.com>
2024-04-12 16:08:33 -0500

    updated scan build list

--- pluggins/scan_list_builder.py
+++ pluggins/scan_list_builder.py
@@ -9,13 +9,19 @@ def convert_to_list(string: str, delimiter: str) -> list:
 def build_k3d_item_dict(string: str) -> list:
     """Build Kumpe3D Item Dict"""
     k3d_dict_list = []
-    k3d_list = convert_to_list(string, "&&")
+    k3d_list = convert_to_list(string, "|")
     for item in k3d_list:
         item_list = convert_to_list(item, ";;")
-        if len(item_list) == 1:
-            item_dict = {"qty": 1, "sku": str(item_list[0])}
-            k3d_dict_list.append(item_dict)
-        else:
-            item_dict = {"qty": int(item_list[0]), "sku": str(item_list[1])}
-            k3d_dict_list.append(item_dict)
+        try:
+            qty = int(item_list[0])
+            sku = str(item_list[1])
+        except (IndexError, ValueError):
+            qty = 1
+            sku = str(item_list[0])
+        try:
+            qr_id = item_list[2]
+        except IndexError:
+            qr_id = None
+        item_dict = {"qty": qty, "sku": sku, "qr_id": qr_id}
+        k3d_dict_list.append(item_dict)
     return k3d_dict_list
