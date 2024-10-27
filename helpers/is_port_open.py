558def3903b41a215b835e5cc282b60cbcce756b
Justin Kumpe <jakumpe@kumpes.com>
2024-10-27 09:55:36 -0500

    -

---
+++ helpers/is_port_open.py
@@ -0,0 +1,16 @@
+"""Server Monitor"""
+import socket
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def rw_sql() -> bool:
+    """Is mySQL port open for rw.sql.pv
+
+    Returns:
+        bool: Server Is Up
+    """    
+    result = sock.connect_ex(("rw.sql.pvt.kumpedns.us", 3306))
+    sock.close()
+    return result == 0
